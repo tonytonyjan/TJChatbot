76ecad3f8c427314ab73de70510baca9752cf42e
Tony Jian <tonytonyjan@gmail.com>
2012-03-26 03:44:34 +0800

    fix add public response bug

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -26,7 +26,7 @@ class CategoriesController < ApplicationController
       return
     end
     
-    user = User.find(params[:category][:user_id])
+    user = User.find(params[:category][:user_id]) if params[:category][:user_id]
     @category = Category.new(params[:category])
     
     # If it's a personal category, user should signed in
@@ -43,7 +43,7 @@ class CategoriesController < ApplicationController
     
     if @category.save
       flash[:success] = "提交成功！"
-      redirect_to params[:category][:user_id] ? user_path(user.nick_name) : categories_path
+      redirect_to user ? user_path(user.nick_name) : categories_path
     else
       flash.now[:error] = "提交失敗！"
       need_patterns = 3 - @category.patterns.size
